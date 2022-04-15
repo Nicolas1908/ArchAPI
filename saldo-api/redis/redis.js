fd168245fd81b3e5f8d58871389d5a183ddda342
Nicolas1908 <73202544+Nicolas1908@users.noreply.github.com>
2022-04-14 22:18:42 -0300

    Update redis.js

--- saldo-api/redis/redis.js
+++ saldo-api/redis/redis.js
@@ -1,9 +1,8 @@
 const Redis = require('ioredis');
 
-
 const redis = new Redis({
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT
+    host: 'localhost',
+    port: 6379
 });
 
 async function get(key) {
